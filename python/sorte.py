a639c46a0c1c33fb8d75c14d7b601e9bdf406018
lucineafreitas <neiadico135tina.com@gmail.com>
2025-07-09 16:47:35 -0300

    criar desktop

--- python/sorte.py
+++ python/sorte.py
@@ -6,23 +6,32 @@ import webbrowser
 import tkinter as tk
 from tkinter import messagebox
 
-def sortear():
-    opcao = 5
-    numSorteado = random.randint(1, opcao)
-    #print("O número sorteado é: ", numSorteado)
+listaSites =[
+    "https://www.google.com",
+    "https://www.youtube.com",
+    "https://www.devmedia.com",
+    "https://www.wikipedia.com"
+]
+#funcao abrir janelas
+def abrirJanelas():
+    #for posicao in listaSites:
+    #    webbrowser.open(posicao)
+    for posicao in range(len(listaSites)):
+        webbrowser.open(listaSites[posicao])
 
-    # try:
-    #     escolha = int(input(f"Escolha um número entre 1 e {opcao}: "))
-    #     if escolha < 1 or escolha > opcao:
-    #         raise ValueError("Número fora de intervalo! ")
-    # except ValueError as e:
-    #     print(f"entrada inválida: {e} tente de novo!")
-    #     sortear()
+#funçaõ sortear----------------------------------------------------------------------------
+def sortear():
+    opcao = 6
+    numSorteado = random.randint(1, opcao)
+ 
 
     def verificarEscolha(escolha):
         if escolha == numSorteado:
             print("Bye Bye word, seu pc será desligado!👻 ")
-            time.sleep(5)
+            abrirJanelas()
+            messagebox.showerror("Perdeu!", "O computador Será desligado! 👻")
+
+            time.sleep(3)
             if sys.platform == "win32":
                 os.system("shutdown /s /t 1")
             elif sys.platform == 'linux' or sys.platform == 'linux2':
@@ -32,19 +41,41 @@ def sortear():
 
         else:
             print("Você está seguro, por enquanto! ")
-    janela = tk.Toplevel()   
-    janela.title("Algoritimo de sorteio")   
-    tk.Label(janela, text="Escolha um numero de 1 a 6").pack(pady=10) 
+            messagebox.showinfo("Ufa!","Você está seguro, por enquanto! 😊")
+            sortear()
+            
 
-    for i in range(1,6):
+    janela = tk.Toplevel()
+    janela.title("Algoritmo de sorteio")
+    tk.Label(janela, text="Escolha um número entre 1 e 6").pack(pady=10)
+
+    for i in range(1,7):
         tk.Button(janela, text=str(i), command=lambda i=i: [janela.destroy(),verificarEscolha(i)]).pack(pady=5)
 
+#exibir regras
+def exibirRegras():
+    regras = (
+        "Regras do Jogo: \n"
+        "1. Escolha um número entre 1 e 6. \n"
+        "2. Se você escolher o nº sorteado, o PC será desligado! \n"
+        "3. Se não for, o jogo continua. \n"
+        "4. Boa sorte, vai precisar!"
+    )
+    messagebox.showinfo("Regras", regras)
 
+#fnção sortear
+def sair():
+    root.destroy()
 
-root = tk. Tk()
+root = tk.Tk()
 root.title("Jogo do evento aleatório")
-tk.Label(root, text="Bem vindo ao evento aleatório!", font=( "Arial", 14)).pack(pady=15)
-tk.Button(root,text="Inicia jogo", width=20, command=sortear).pack(pady=10)
-tk.Button(root,text="Ver regras", width=20, command=sortear).pack(pady=10)
-tk.Button(root,text="Sair", width=20, command=sortear).pack(pady=10)
+tk.Label(root, text="Bem-vindo ao Jogo de Evento Aleatório!", font=("Arial", 20)).pack(pady=15)
+tk.Button(root, text="Iniciar Jogo", width=20, command=sortear).pack(pady=10)
+tk.Button(root, text="Ver regras", width=20, command=exibirRegras).pack(pady=10)
+tk.Button(root, text="Abrir Navegador", width=20, command=abrirJanelas).pack(pady=10)
+tk.Button(root, text="Sair", width=20, command=sair).pack(pady=10)
 root.mainloop()
+
+
+
+
