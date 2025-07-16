83c62360f889cf7d3b8907f2b5a47d406b10637b
lucineafreitas <neiadico135tina.com@gmail.com>
2025-07-16 17:33:13 -0300

    tela loguin

--- sistema/script.js
+++ sistema/script.js
@@ -1,19 +1,36 @@
 let table = new DataTable('#tabela', {
     // options
+     language: {
+        url: 'https://cdn.datatables.net/plug-ins/2.3.2/i18n/pt-BR.json',
+    },
 });
 
-document.getElementById("toggleSenha").addEventListener("click", function () {
-    const senha = document.getElementById("senha");
-    const icone = document.getElementById("iconeOlho");
+    // function visualizar(){
+    //     var senha = $('senha');
+    //     var icone = $('#olho');
 
-    if (senha.type === "password") {
-        senha.type = "text";
-        icone.classList.remove("fa-eye");
-        icone.classList.add("fa-eye-slash");
-    } else {
-        senha.type = "password";
-        icone.classList.remove("fa-eye-slash");
-        icone.classList.add("fa-eye");
-    }
+    //     if(senha.attr('type') === 'password'){
+    //         senha.attr('type, text');
+    //         icone.removeClass('fa-eye'),addclass('fa-eye-slash');  
+    //     }else{
+    //         senha.attr('type', 'password');
+    //         icone.removeClass('fa-eye-slash').addClass('fa-eye');
+
+    //     }
+    // }
+
+document.getElementById("toggleSenha").addEventListener("click", function () {
+   const senha = document.getElementById("senha");
+  const icone = document.getElementById("iconeOlho");
+
+   if (senha.type === "password") {
+      senha.type = "text";
+       icone.classList.remove("fa-eye");
+      icone.classList.add("fa-eye-slash");
+   } else {
+       senha.type = "password";
+     icone.classList.remove("fa-eye-slash");
+      icone.classList.add("fa-eye");
+   }
 });
 
